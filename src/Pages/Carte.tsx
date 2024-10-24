5820b0910b58c069040d25b26f11640a16f9758e
cramara <gabin.joussot-dubien@insa-lyon.fr>
2024-10-24 18:27:17 +0200

    optimisation de l'affichage des points et changement de la couleur
    Took 17 minutes

--- src/Pages/Carte.tsx
+++ src/Pages/Carte.tsx
@@ -1,18 +1,17 @@
-// @ts-ignore
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polygon, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
-import * as turf from '@turf/turf'; // Import de Turf.js
+import * as turf from '@turf/turf';
 import { Button } from '@mui/material';
 import { Point, Intersection } from '../Utils/points';
 import 'leaflet/dist/leaflet.css';
 
 interface CarteProps {
-    intersections: Intersection[]; // Typage des intersections reçues en props
-    adressesLivraisonsAjoutees: Intersection[]; // Typage des livraisons ajoutées manuellement
-    adressesLivraisonsXml: Intersection[]; // Typage des livraisons provenant du fichier XML
-    setAdresseLivraisonsAjoutees: (adresses: Intersection[]) => void; // Setter pour ajouter les livraisons
-    adresseEntrepot: Intersection | null; // Typage de l'entrepôt (un seul entrepôt, ou null)
+    intersections: Intersection[];
+    adressesLivraisonsAjoutees: Intersection[];
+    adressesLivraisonsXml: Intersection[];
+    setAdresseLivraisonsAjoutees: (adresses: Intersection[]) => void;
+    adresseEntrepot: Intersection | null;
 }
 
 const customMarkerIntersections = new L.Icon({
@@ -48,26 +47,24 @@ const polygonStyle = {
 };
 
 const Carte: React.FC<CarteProps> = ({
-                                         intersections,
-                                         adressesLivraisonsAjoutees,
-                                         adressesLivraisonsXml,
-                                         setAdresseLivraisonsAjoutees,
-                                         adresseEntrepot
-                                     }) => {
+    intersections,
+    adressesLivraisonsAjoutees,
+    adressesLivraisonsXml,
+    setAdresseLivraisonsAjoutees,
+    adresseEntrepot
+}) => {
     const [zoomLevel, setZoomLevel] = useState<number>(13);
     const [convexHull, setConvexHull] = useState<any>(null);
     const [intersectionsFiltrees, setIntersectionsFiltrees] = useState<Intersection[]>([]);
-    
+    const [mapBounds, setMapBounds] = useState<L.LatLngBounds | null>(null);
+
     const minZoomLevelForIntersections = 16;
-    
-    const ajouterBouton = (id: number, longitude:number, latitude:number, adresse: string) => {
+
+    const ajouterBouton = (id: number, longitude: number, latitude: number, adresse: string) => {
         const adresseExiste = adressesLivraisonsAjoutees.some((livraison) => livraison.id === id);
         if (!adresseExiste) {
-            setAdresseLivraisonsAjoutees([...adressesLivraisonsAjoutees, 
-                { id: id, 
-                longitude: longitude, 
-                latitude: latitude, 
-                adresse: adresse }]);
+            setAdresseLivraisonsAjoutees([...adressesLivraisonsAjoutees,
+                { id: id, longitude: longitude, latitude: latitude, adresse: adresse }]);
         }
     };
 
@@ -75,22 +72,25 @@ const Carte: React.FC<CarteProps> = ({
         useMapEvents({
             zoomend: (e) => {
                 setZoomLevel(e.target.getZoom());
+                setMapBounds(e.target.getBounds());
             },
+            moveend: (e) => {
+                setMapBounds(e.target.getBounds());
+            }
         });
         return null;
     };
 
     useEffect(() => {
-
-        // Permet de filtre les intersections pour ne pas afficher celles déjà ajoutées
         const filteredIntersections = intersections.filter(intersection => {
             const isInAdressesLivraisonsXMLs = adressesLivraisonsXml.some(livraison => livraison.id === intersection.id);
             const isEntrepot = adresseEntrepot && adresseEntrepot.id === intersection.id;
             const isAdresseLivraisonsAjoutees = adressesLivraisonsAjoutees.some(livraison => livraison.id === intersection.id);
-            return !isInAdressesLivraisonsXMLs && !isEntrepot && !isAdresseLivraisonsAjoutees;
+            const isInBounds = mapBounds ? mapBounds.contains([intersection.latitude, intersection.longitude]) : true;
+            return !isInAdressesLivraisonsXMLs && !isEntrepot && !isAdresseLivraisonsAjoutees && isInBounds;
         });
         setIntersectionsFiltrees(filteredIntersections);
-        
+
         const allPoints = [...intersections, ...adressesLivraisonsXml];
         if (adresseEntrepot) {
             allPoints.push(adresseEntrepot);
@@ -111,7 +111,7 @@ const Carte: React.FC<CarteProps> = ({
             }
         }
 
-    }, [intersections, adressesLivraisonsXml, adresseEntrepot, adressesLivraisonsAjoutees]);
+    }, [intersections, adressesLivraisonsXml, adresseEntrepot, adressesLivraisonsAjoutees, mapBounds]);
 
     return (
         <MapContainer center={[45.75, 4.85]} zoom={zoomLevel} style={{ height: '400px', width: '100%' }}>
@@ -119,14 +119,13 @@ const Carte: React.FC<CarteProps> = ({
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-
             {zoomLevel >= minZoomLevelForIntersections && intersectionsFiltrees.map((intersection) => (
                 <Marker key={intersection.id} position={[intersection.latitude, intersection.longitude]} icon={customMarkerIntersections}>
                     <Popup>
                         <span>{`Intersection ID: ${intersection.id}`}</span>
                         <br />
-                        <span><b>{`Adresse : ${intersection.adresse}`}</b><br/></span>
-                        <Button onClick={() => 
+                        <span><b>{`Adresse : ${intersection.adresse}`}</b><br /></span>
+                        <Button onClick={() =>
                             ajouterBouton(intersection.id, intersection.longitude, intersection.latitude, intersection.adresse)}>Ajouter</Button>
                     </Popup>
                 </Marker>
@@ -137,7 +136,7 @@ const Carte: React.FC<CarteProps> = ({
                     <Popup>
                         <span>{`Entrepôt ID: ${adresseEntrepot.id}`}</span>
                         <br />
-                        <span><b>{`Adresse de l'entrepôt : ${adresseEntrepot.adresse}`}</b><br/></span>
+                        <span><b>{`Adresse de l'entrepôt : ${adresseEntrepot.adresse}`}</b><br /></span>
                     </Popup>
                 </Marker>
             )}
@@ -147,23 +146,22 @@ const Carte: React.FC<CarteProps> = ({
                     <Popup>
                         <span>{`Livraison ID: ${livraison.id}`}</span>
                         <br />
-                        <span><b>{`Adresse de livraison : ${livraison.adresse}`}</b><br/></span>
+                        <span><b>{`Adresse de livraison : ${livraison.adresse}`}</b><br /></span>
                     </Popup>
                 </Marker>
             ))}
-            
+
             {adressesLivraisonsAjoutees && adressesLivraisonsAjoutees.map((livraison) => (
                 <Marker key={livraison.id} position={[livraison.latitude, livraison.longitude]} icon={customMarkerLivraisonAjoutee}>
                     <Popup>
                         <span>{`Livraison ID: ${livraison.id}`}</span>
                         <br />
-                        <span><b>{`Adresse de livraison : ${livraison.adresse}`}</b><br/></span>
+                        <span><b>{`Adresse de livraison : ${livraison.adresse}`}</b><br /></span>
                     </Popup>
                 </Marker>
             ))}
-
             {convexHull && (
-                <Polygon positions={convexHull} pathOptions={polygonStyle}/>
+                <Polygon positions={convexHull} pathOptions={polygonStyle} />
             )}
         </MapContainer>
     );
